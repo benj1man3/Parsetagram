fb913aa653f46d8a9f31944ec711cc848468ab75
Benjamin Rodriguez <benj1man3@fb.com>
2018-07-11 16:02:53 -0700

    This commit allows to post a description and logs it in Parse.

--- app/src/main/java/me/benj1man3/mainactivity/ParseApp.java
+++ app/src/main/java/me/benj1man3/mainactivity/ParseApp.java
@@ -3,6 +3,9 @@ package me.benj1man3.mainactivity;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
+
+import me.benj1man3.mainactivity.model.Post;
 
 public class ParseApp extends Application{
 
@@ -10,11 +13,14 @@ public class ParseApp extends Application{
     public void onCreate() {
         super.onCreate();
 
+        ParseObject.registerSubclass(Post.class);
+
         final Parse.Configuration configuration= new Parse.Configuration.Builder(this)
                 .applicationId("benjamin")
                 .clientKey("benjiman4573")
                 .server("http://benj1man3-fbu-instagram.herokuapp.com/parse")
                 .build();
+
         Parse.initialize(configuration);
 
     }
