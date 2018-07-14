b353773538c71e16ce6c2479a491ef9911b05b93
Benjamin Rodriguez <benj1man3@fb.com>
2018-07-13 17:42:39 -0700

    Added the logout feature.

--- app/src/main/java/me/benj1man3/mainactivity/ProfileFragment.java
+++ app/src/main/java/me/benj1man3/mainactivity/ProfileFragment.java
@@ -1,12 +1,18 @@
 package me.benj1man3.mainactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.parse.ParseUser;
 
 public class ProfileFragment extends Fragment {
+    private ImageButton logoutButton;
+
     // The onCreateView method is called when Fragment should create its View object hierarchy,
     // either dynamically or via XML layout inflation.
     @Override
@@ -21,5 +27,21 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Setup any handles to view objects here
         // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+        logoutButton =view.findViewById(R.id.outButton);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("PLEASE LOGOUT");
+                ParseUser user=ParseUser.getCurrentUser();
+                ParseUser.logOut();
+
+                ParseUser currentUser = ParseUser.getCurrentUser();
+
+                Intent intent= new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
